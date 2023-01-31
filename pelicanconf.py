611afb33b1a60511f985feb7757297a0834b52df
Andy McKay <andy@clearwind.ca>
2023-01-31 14:21:27 -0800

    new theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,8 +1,33 @@
 AUTHOR = 'Andy McKay'
 SITENAME = 'Ban Cars'
-SITEURL = 'https://andymckay.github.io/ban-cars/'
+SITEURL = ''
+SUBTITLE = 'Ban Cars'
+SUBTEXT = 'About cars and why we need less of them.'
+COPYRIGHT = '2023 and beyond'
 
 PATH = 'content'
+THEME = 'themes/Papyrus'
+THEME_STATIC_PATHS = ['static']
+PLUGIN_PATHS = ['pelican-plugins']
+PLUGINS = ['readimte', 'search', 'neighbors', 'pelican-toc']
+
+DISPLAY_PAGES_ON_MENU = True
+DIRECT_TEMPLATES = (('index', 'search', 'tags', 'categories', 'archives',))
+PAGINATED_TEMPLATES = {'index':None,'tag':None,'category':None,'author':None,'archives':24,}
+
+# Site search plugin
+SEARCH_MODE = "output"
+SEARCH_HTML_SELECTOR = "main"
+# Table of Content Plugin
+TOC = {
+    'TOC_HEADERS'       : '^h[1-3]', # What headers should be included in
+                                     # the generated toc
+                                     # Expected format is a regular expression
+    'TOC_RUN'           : 'true',    # Default value for toc generation,
+                                     # if it does not evaluate
+                                     # to 'true' no toc will be generated
+    'TOC_INCLUDE_TITLE': 'false',    # If 'true' include title in toc
+}
 
 TIMEZONE = 'America/Vancouver'
 
@@ -21,9 +46,21 @@ LINKS = (('Pelican', 'https://getpelican.com/'),
          ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
          ('You can modify those links in your config file', '#'),)
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# Social widgets
+SOCIAL = (
+    ('github', 'https://github.com/andymckay/ban-cars'),
+    ('twitter', 'https://twitter.com/'),
+)
+
+# Article share widgets
+SHARE = (
+    ("twitter", "https://twitter.com/intent/tweet/?text=Features&amp;url="),
+    ("linkedin", "https://www.linkedin.com/sharing/share-offsite/?url="),
+    ("reddit", "https://reddit.com/submit?url="),
+    ("facebook", "https://facebook.com/sharer/sharer.php?u="),
+    ("whatsapp", "https://api.whatsapp.com/send?text=Features - "),
+    ("telegram", "https://telegram.me/share/url?text=Features&amp;url="),
+)
 
 DEFAULT_PAGINATION = 10
 
